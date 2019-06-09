1331531f0c4553110d4e7fd74051ec16ace7e2d4
Erik <graphics.erik@gmail.com>
2019-06-09 01:01:08 -0400

    Add emsdk version of last successful build

--- src/build_all.bat
+++ src/build_all.bat
@@ -1,4 +1,5 @@
 :: Requires Emscripten to build: https://kripken.github.io/emscripten-site/docs/getting_started/downloads.html
+:: Successfully built with emsdk 1.38.11
 call emcc -std=c++11 hello_triangle.cpp events.cpp camera.cpp -s USE_SDL=2 -s FULL_ES2=1 -s WASM=0 -o ..\hello_triangle.js
 call emcc -std=c++11 hello_texture.cpp events.cpp camera.cpp -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS="[""png""]" -s FULL_ES2=1 -s WASM=0 --preload-file media/texmap.png -o ..\hello_texture.js
 call emcc -std=c++11 hello_text_ttf.cpp events.cpp camera.cpp -s USE_SDL=2 -s USE_SDL_TTF=2 -s FULL_ES2=1 -s WASM=0 --preload-file media/LiberationSansBold.ttf -o ..\hello_text_ttf.js
